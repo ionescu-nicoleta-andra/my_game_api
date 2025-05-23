9e400a440f45e110be7a22aabf4de586a76bcc19
ionescu-nicoleta-andra <ionescu.nicoleta.andra@gmail.com>
2025-05-23 17:44:02 +0300

    added fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Role;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Create roles
+        $userRole = new Role();
+        $userRole->setName('ROLE_USER');
+        $manager->persist($userRole);
+
+        $adminRole = new Role();
+        $adminRole->setName('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        // Create users
+        $nico = new User();
+        $nico->setEmail('nico@example.com');
+        $nico->setPassword($this->hasher->hashPassword($nico, 'password123'));
+
+        $nico->setRoles([$userRole]);
+        $manager->persist($nico);
+
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setPassword($this->hasher->hashPassword($admin, 'adminpass'));
+        $admin->setRoles([$adminRole]);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
